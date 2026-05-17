9449952b0d97405a8f494724aef724f123db39c4
Bernardette Aragon <BSARAGON@correo.url.edu.gt>
2026-05-16 19:02:29 -0600

    Nodo arbol

--- src/java/modelo/NodoArbol.java
+++ src/java/modelo/NodoArbol.java
@@ -6,7 +6,17 @@ package modelo;
 
 import java.util.ArrayList;
     
-public class NodoArbol 
-{
+public class NodoArbol {
     
+    int idUsuario;
+    int posicionHash;
+    
+    ArrayList<NodoArbol> hijos;
+    
+    public NodoArbol(int idUsuario, int posicionHash)
+    {
+        this.idUsuario = idUsuario;
+        this.posicionHash = posicionHash;
+        hijos = new ArrayList<>();
+    }
 }
