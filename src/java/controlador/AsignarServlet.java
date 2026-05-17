7ccac4a9b1f2ded03e4ed8bc57d3242746c259f5
nix-xd <nix.extraxd@gmail.com>
2026-05-17 04:41:35 -0600

    Update AsignarServlet.java

--- src/java/controlador/AsignarServlet.java
+++ src/java/controlador/AsignarServlet.java
@@ -4,10 +4,27 @@
  */
 package controlador;
 
-/**
- *
- * @author berna
- */
-public class AsignarServlet {
-    
+import modelo.PedidoDAO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet("/AsignarServlet")
+
+public class AsignarServlet extends HttpServlet 
+{
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException 
+    {
+        int pedidoId = Integer.parseInt(request.getParameter("pedidoId"));
+        int repartidorId = Integer.parseInt(request.getParameter("repartidorId"));
+
+        PedidoDAO dao = new PedidoDAO();
+
+        dao.asignarRepartidor(pedidoId, repartidorId);
+
+        response.sendRedirect("admin.jsp");
+    }
 }
